3e319db3ad4c02854892c91d30e1e277267d5f9b
rochadev <rochasantosvideos@gmail.com>
2024-04-30 21:38:57 -0300

    atualizar

--- meu portfólio/js/index.js
+++ meu portfólio/js/index.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("scroll", function () {
         const scrollPosition = window.scrollY;
 
-        const scrollThreshold = 1200;
+        const scrollThreshold = 1500;
 
         if (scrollPosition > scrollThreshold) {
             header.classList.add("scrolled");
@@ -82,6 +82,52 @@ setTimeout(() => {
 setTimeout(() => {
     showText(elements.h3, phrasesH3, interval);
 }, 2500);
+
+
+/* FUNÇÃO PROJETOS DIV QUE SE ABRE */
+
+
+const primeiradiv = document.getElementById("primeiradiv");
+const segundadiv = document.getElementById("segundadiv");
+const button5 = document.getElementById("button");
+const pai = document.getElementById("pai");
+
+
+
+
+primeiradiv.addEventListener('mouseover', function () {
+    segundadiv.style.top = "0px";
+    segundadiv.style.opacity = "1";
+});
+
+
+segundadiv.addEventListener('mouseover', function () {
+    segundadiv.style.top = "0px";
+    segundadiv.style.opacity = "1";
+});
+
+segundadiv.addEventListener('mouseout', function () {
+    segundadiv.style.top = "-25%";
+    segundadiv.style.opacity = "0";
+});
+
+
+primeiradiv.addEventListener('mouseout', function () {
+    segundadiv.style.top = "-25%";
+    segundadiv.style.opacity = "1";
+});
+
+pai.addEventListener('mouseover', function () {
+    button5.style.transform = "scale(1.3)";
+    button5.style.transition = "all 2s ease-in-out";
+    button5.style.backgroundColor = "#24abfa";
+});
+
+pai.addEventListener('mouseout', function () {
+    button5.style.transition = "none";
+    button5.style.transform = "none";
+    button5.style.backgroundColor = "transparent";
+});
 
 
 /* VOLTAR AO TOPO */
